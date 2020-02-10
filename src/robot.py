c943b9cf1f1a3085148ccac282a160002a468b04
Brandon Werbel <brandon@werbel.org>
2020-02-10 17:39:51 -0500

    Uses correct motors for robot

--- src/robot.py
+++ src/robot.py
@@ -1,14 +1,14 @@
 import magicbot
 import wpilib
+from wpilib import VictorSP
 from magicbot import tunable
 from robotpy_ext.control.toggle import Toggle
 from wpilib.buttons import JoystickButton
 
 from common.ctre import WPI_TalonSRX, WPI_VictorSPX
 from wpilib.kinematics import DifferentialDriveKinematics
-from common.rev import CANSparkMax, IdleMode, MotorType
+from common.rev import IdleMode, MotorType
 from components import Drive, Intake
-from common.camera_server import CameraServer
 
 r"""
 / \                / \
@@ -33,8 +33,6 @@ class Robot(magicbot.MagicRobot):
     drive: Drive
     intake: Intake
 
-    TRACK_WIDTH = 0.43  # Units: Meters
-
     def createObjects(self):
         # Joysticks
         self.joystick_left = wpilib.Joystick(0)
@@ -42,65 +40,32 @@ class Robot(magicbot.MagicRobot):
         self.joystick_alt = wpilib.Joystick(2)
 
         # Buttons
-        self.btn_intake_in = JoystickButton(self.joystick_alt, 3)
-        self.btn_intake_out = JoystickButton(self.joystick_alt, 5)
-        self.btn_slow_movement = Toggle(self.joystick_right, 3)
+        self.btn_intake_in = JoystickButton(self.joystick_right, 1)
 
         # Set up Speed Controller Groups
         self.left_motors = wpilib.SpeedControllerGroup(
-            CANSparkMax(1, MotorType.kBrushless),
-            CANSparkMax(2, MotorType.kBrushless),
+            VictorSP(0),
+            VictorSP(1),
         )
 
         self.right_motors = wpilib.SpeedControllerGroup(
-            CANSparkMax(3, MotorType.kBrushless),
-            CANSparkMax(4, MotorType.kBrushless),
+            VictorSP(2),
+            VictorSP(3),
         )
 
         # Drivetrain
         self.train = wpilib.drive.DifferentialDrive(self.left_motors, self.right_motors)
 
         # Intake
-        self.intake_motor = WPI_VictorSPX(1)
-
-        # Kinematics
-        self.kinematics = DifferentialDriveKinematics(self.TRACK_WIDTH)  # Track width in meters
-
-
-    def teleopInit(self):
-        self.drive.speed_constant = 1.05
-        self.drive.rotational_constant = 0.5
+        self.intake_motor = VictorSP(4)
 
     def teleopPeriodic(self):
         self.drive.move(-self.joystick_left.getY(),
                         self.joystick_right.getX())
 
-        if self.btn_slow_movement:
-            # 10% of original values
-            self.drive.rotational_constant = 0.05
-            self.drive.speed_constant = 0.105
-        else:
-            self.drive.rotational_constant = 0.5
-            self.drive.speed_constant = 1.05
-
         # Intake
-        if self.btn_intake_out.get():
-            self.intake.spin(1)
-        elif self.btn_intake_in.get():
+        if self.btn_intake_in.get():
             self.intake.spin(-1)
-
-        # #slow movement using POV on joystick_alt
-        # if self.joystick_alt.getPOV() == 0:
-        #     self.drive.move(0.2, 0)
-        # elif self.joystick_alt.getPOV() == 180:
-        #     self.drive.move(-0.2, 0)
-        # elif self.joystick_akt.getPOV() == 90:
-        #     self.drive.move(0, -0.2)
-        # elif self.joystick_alt.getPOV() == 270:
-        #     self.drive.move(0, 0.2)
-
-        # print(self.joystick_alt.getPOV())
-
 
 if __name__ == '__main__':
     wpilib.run(Robot)
